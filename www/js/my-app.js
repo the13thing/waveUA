4b2363a0d78a584390609441f0e22664aa4a00e7
the13thing <the13thing@hotmail.com>
2016-05-30 15:47:03 +0100

    wave

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,3 +1,122 @@
+angular
+    .module('waveUA', ['spotify'])
+    .config(function (SpotifyProvider) {
+        SpotifyProvider.setClientId('d1fa368a055b41bb95664fc3cb7a719e');
+        SpotifyProvider.setRedirectUri('http://wave.web.ua.pt/www/callback.html');
+        SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
+    })
+    .controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
+
+        $scope.searchArtist = function () {
+            Spotify.search($scope.searchartist, 'artist').then(function (data) {
+                $scope.artists = data.artists.items;
+            });
+        };
+        $scope.showProfile = function () {
+            Spotify.getCurrentUser().then(function (data) {
+
+            });
+        };
+        $scope.login = function () {
+            Spotify.login().then(function (data) {
+                Spotify.getCurrentUser().then(function (data) {
+                    $("#profile_name").append(data.display_name);
+                    $("#profile_country").append(data.country);
+                    $("#profile_image").attr("src",data.images[0].url);
+                    $("#profileSide").attr("src",data.images[0].url);
+                    $("#profileName").append(data.display_name);
+
+                });
+                mainView.router.load({pageName: 'index'});
+
+            }, function () {
+                mainView.router.load({pageName: 'index'});
+                console.log('didn\'t log in');
+            })
+        };
+        /*
+         // Gets an album
+         Spotify.getAlbum('0sNOF9WDwhWunNAHPD3Baj').then(function (data){
+         console.log('=================== Album - ID ===================');
+         console.log(data);
+         });
+         // Works with Spotify uri too
+         Spotify.getAlbum('spotify:album:0sNOF9WDwhWunNAHPD3Baj').then(function (data){
+         console.log('=================== Album - Spotify URI ===================');
+         console.log(data);
+         });
+
+         //Get multiple Albums
+         Spotify.getAlbums('41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4,6UXCm6bOO4gFlDQZV5yL37').then(function (data) {
+         console.log('=================== Albums - Ids ===================');
+         console.log(data);
+         });
+         Spotify.getAlbums(['41MnTivkwTO3UUJ8DrqEJJ','6JWc4iAiJ9FjyK0B59ABb4','6UXCm6bOO4gFlDQZV5yL37']).then(function (data) {
+         console.log('=================== Albums - Array ===================');
+         console.log(data);
+         });
+
+
+         Spotify.getAlbumTracks('41MnTivkwTO3UUJ8DrqEJJ').then(function (data) {
+         console.log('=================== Album Tracks - ID ===================');
+         console.log(data);
+         });
+         Spotify.getAlbumTracks('spotify:album:41MnTivkwTO3UUJ8DrqEJJ').then(function (data) {
+         console.log('=================== Album Tracks - Spotify URI ===================');
+         console.log(data);
+         });
+
+
+
+         //Artist
+         Spotify.getArtist('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
+         console.log('=================== Artist - Id ===================');
+         console.log(data);
+         });
+         Spotify.getArtist('spotify:artist:0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
+         console.log('=================== Artist - Spotify URI ===================');
+         console.log(data);
+         });
+
+         Spotify.getArtistAlbums('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
+         console.log('=================== Artist Albums - Id ===================');
+         console.log(data);
+         });
+
+         Spotify.getArtistAlbums('spotify:artist:0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
+         console.log('=================== Artist Albums - Spotify URI ===================');
+         console.log(data);
+         });
+
+         Spotify.getArtistTopTracks('0LcJLqbBmaGUft1e9Mm8HV', 'AU').then(function (data) {
+         console.log('=================== Artist Top Tracks Australia ===================');
+         console.log(data);
+         });
+
+         Spotify.getRelatedArtists('0LcJLqbBmaGUft1e9Mm8HV').then(function (data) {
+         console.log('=================== Get Releated Artists ===================');
+         console.log(data);
+         });
+
+
+         //Tracks
+         Spotify.getTrack('0eGsygTp906u18L0Oimnem').then(function (data) {
+         console.log('=================== Track ===================');
+         console.log(data);
+         });
+
+         Spotify.getTracks('0eGsygTp906u18L0Oimnem,1lDWb6b6ieDQ2xT7ewTC3G').then(function (data) {
+         console.log('=================== Tracks - String ===================');
+         console.log(data);
+         });
+
+         Spotify.getTracks(['0eGsygTp906u18L0Oimnem','1lDWb6b6ieDQ2xT7ewTC3G']).then(function (data) {
+         console.log('=================== Tracks - Array ===================');
+         console.log(data);
+         });*/
+
+    }]);
+
 //Transform JSON into string
 Template7.registerHelper('stringify', function (context){
     var str = JSON.stringify(context);
@@ -196,6 +315,7 @@ $$(document).on('click', '#about', function (e) {
 });
 $$(document).on('click', '#mapMenu', function(e){
     mainView.router.load({pageName: 'map'});
+    initMap();
 
 });
 $$(document).on('click', '#feedMenu', function(e){
@@ -237,9 +357,53 @@ myApp.onPageInit ('media', function (page) {
 });
 
 // MAP AND GEOLOCATION
+    initMap();
+    function initMap() {
+        var onSuccess = function(position) {
+            var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            var options = {
+                zoom: 15,
+                center: coords,
+                mapTypeControl: false,
+                navigationControlOptions: {
+                    style: google.maps.NavigationControlStyle.SMALL
+                }
+
+            };
+            var map = new google.maps.Map(document.getElementById('map-canvas'), options);
+            var marker = new google.maps.Marker({
+                position: coords,
+                map: map,
+                title: "You are here!"
+            });
+        };
+        var onError = function(error){var options = {
+            zoom: 15,
+            center: {lat: -34.397, lng: 150.644},
+            mapTypeControl: false,
+            navigationControlOptions: {
+                style: google.maps.NavigationControlStyle.SMALL
+            }
+
+        };
+            var map = new google.maps.Map(document.getElementById('map-canvas'), options);
+            var marker = new google.maps.Marker({
+                position: {lat: -34.397, lng: 150.644},
+                map: map,
+                title: "You are here!"
+            });
+            window.alert('Code:'+error.code+'\n'+'message:'+error.message+'\n');
+        };
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+            $$(document).on('deviceready', function() {
+                navigator.geolocation.getCurrentPosition(onSuccess,onError, {timeout: 10000, enableHighAccuracy: true});
+            });        }
+        else {
+            navigator.geolocation.getCurrentPosition(onSuccess,onError, {timeout: 10000, enableHighAccuracy: true});
+        }
 
 
-    
+    }
 //OPEN WINDOWS IN POPUP (GOOD FOR DATABASE STUFF)
 function popupform(myform, windowname)
 {
